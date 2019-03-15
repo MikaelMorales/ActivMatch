e98fb51234912fb3de2f2bb697e7f4399e24e527
MikaelMorales <morales.mikael@outlook.com>
2019-03-15 13:18:55 +0100

    Finished mock storage

--- android/ActivMatch/app/src/main/java/ch/unil/eda/activmatch/models/Message.java
+++ android/ActivMatch/app/src/main/java/ch/unil/eda/activmatch/models/Message.java
@@ -1,16 +1,26 @@
 package ch.unil.eda.activmatch.models;
 
 public class Message {
+    private String mMessageId;
     private String mGroupId;
     private String mText;
     private User mCreator;
     private String mDate;
 
-    public Message(String groupId, String date, String text, User creator) {
+    public Message(String messageId, String groupId, String date, String text, User creator) {
+        mMessageId = messageId;
         mGroupId = groupId;
         mDate = date;
         mText = text;
         mCreator = creator;
+    }
+
+    public String getMessageId() {
+        return mMessageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.mMessageId = messageId;
     }
 
     public String getGroupId() {
